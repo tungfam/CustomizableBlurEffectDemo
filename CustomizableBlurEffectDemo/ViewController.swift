b0455b514572b7399aa050f666c3bad7cbfae366
Tung Fam <tungfam8@gmail.com>
2018-07-01 12:50:31 +0300

    Initial Commit

---
+++ CustomizableBlurEffectDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomizableBlurEffectDemo
+//
+//  Created by Tung Fam on 7/1/18.
+//  Copyright © 2018 Tung Fam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
